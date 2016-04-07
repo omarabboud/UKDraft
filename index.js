370f19dbbc9f5067d63ed5bde343d0869a48db3a
Annie Jiao <anniej@mit.edu>
2016-04-07 16:30:42 -0400

    labels are up

--- index.js
+++ index.js
@@ -40,27 +40,34 @@ $(function() {
 
     //     });
     // }
-    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+    var margin = { top: 80, right: 20, bottom: 20, left: 80 };
 
-    var x = d3.scale.ordinal().domain(["A", "B", "C", "D", "E", "F", "G"]).rangePoints([0, width]);
+    var width = 500 - margin.left - margin.right;
+    var height = width * 12 / 7;
+
+    var x = d3.scale.ordinal().domain(["A", "B", "C", "D", "E", "F", "G"]).rangePoints([width / 7, width]);
 
     var ydomain = ["1.1", "1.2", "1.3", "2.1", "2.2", "2.3", "3.1", "3.2", "3.3", "4.1", "4.2", "4.3"];
     var y = d3.scale.ordinal().domain(ydomain)
-        .rangePoints([height, 0]);
+        .rangePoints([height / 12, height]);
 
-    var r = d3.scale.linear().range([0, height/10]);
+    var r = d3.scale.linear().range([10, height / 10]);
 
     var color = d3.scale.category10();
 
     var xAxis = d3.svg.axis()
         .scale(x)
-        .orient("bottom");
+        .orient("top")
+        .innerTickSize(-height)
+        .outerTickSize(0)
+        .tickPadding(10);
 
     var yAxis = d3.svg.axis()
         .scale(y)
-        .orient("left");
+        .orient("left")
+        .innerTickSize(-width)
+        .outerTickSize(0)
+        .tickPadding(10);
 
     var svg = d3.select("body").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -70,14 +77,12 @@ $(function() {
 
     d3.json("SIC.json", function(error, data) {
         if (error) throw error;
-        console.log(data);
         var maxFirmCount = 0;
         data.forEach(function(d, i) {
-            
             if (d.activity.length > 1) {
                 var activities = d.activity.replace(/\s+/g, '').split(",");
                 var resources = d.resource.replace(/\s+/g, '').split(",");
-                maxFirmCount=Math.max(maxFirmCount, d.firms/activities.length);
+                maxFirmCount = Math.max(maxFirmCount, d.firms / activities.length);
                 for (var i = 0; i < activities.length; i++) {
                     var entry = {
                         "year": d.year,
@@ -90,7 +95,7 @@ $(function() {
                     data.push(entry);
                 }
             } else {
-              maxFirmCount=Math.max(maxFirmCount, d.firms);
+                maxFirmCount = Math.max(maxFirmCount, d.firms);
             }
         });
         r.domain([0, maxFirmCount]);
@@ -104,25 +109,26 @@ $(function() {
 
         svg.append("g")
             .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
+            .attr("transform", "translate(0," + 10 + ")")
             .call(xAxis)
             .append("text")
             .attr("class", "label")
-            .attr("x", width)
-            .attr("y", -6)
-            .style("text-anchor", "end")
-            .text("Sepal Width (cm)");
+            .attr("y", -10)
+            .attr("transform", "rotate(-90) translate(0,40)")
+            .style("text-anchor", "beginning")
+            .text("Resource");
+
 
         svg.append("g")
             .attr("class", "y axis")
+            .attr("transform", "translate(0," + 10 + ")")
             .call(yAxis)
             .append("text")
             .attr("class", "label")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
+            .attr("y", -10)
+            .attr("transform", "translate(0,30)")
             .style("text-anchor", "end")
-            .text("Sepal Length (cm)")
+            .text("Activity");
 
         svg.selectAll(".dot")
             .data(data)
@@ -132,39 +138,16 @@ $(function() {
                 return r(d.firms);
             })
             .attr("cx", function(d) {
-              // console.log(d.resource.charAt(0), x(d.resource.charAt(0)))
                 return x(d.resource.charAt(0));
             })
             .attr("cy", function(d) {
-                console.log(d.activity.substring(0, 3), y(d.activity.substring(0, 3)));
                 return y(d.activity.substring(0, 3));
             })
-            .style("fill", function(d) {
-                return color(d.species);
-            });
+            .style("fill", "transparent");
 
-        var legend = svg.selectAll(".legend")
-            .data(color.domain())
-            .enter().append("g")
-            .attr("class", "legend")
-            .attr("transform", function(d, i) {
-                return "translate(0," + i * 20 + ")";
-            });
-
-        legend.append("rect")
-            .attr("x", width - 18)
-            .attr("width", 18)
-            .attr("height", 18)
-            .style("fill", color);
-
-        legend.append("text")
-            .attr("x", width - 24)
-            .attr("y", 9)
-            .attr("dy", ".35em")
-            .style("text-anchor", "end")
-            .text(function(d) {
-                return d;
-            });
+        svg.selectAll("text.label")
+           .style("font-size","16px")
+           .style("font-family", "avenir")
 
     });
 })
