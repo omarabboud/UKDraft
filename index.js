557e53f41feaf610f5e12e1f72ea2b66f07fd388
Annie Jiao <anniej@mit.edu>
2016-04-08 12:21:03 -0400

    fixed data incorrections

--- index.js
+++ index.js
@@ -1,57 +1,17 @@
 $(function() {
-    // var SIC_KEY = {};
-    // var plotData = null;
-    // Tabletop.init({
-    //     key: 'https://docs.google.com/spreadsheets/d/1QVe47-yrv5Q_hs2Xd1MBovY3L6Fbnt8N6RMx0Jytq3E/pubhtml?gid=1019022566&single=true',
-    //     callback: function(data, tabletop) {
-    //         for (var i = 0; i < data.length; i++) {
-    //             var sic = data[i].SIC;
-    //             SIC_KEY[sic] = { "activity": data[i].activity, "resource": data[i].resource };
-    //         }
-    //         // console.log(SIC_KEY)
-    //         plotData = parseSIC(SIC_KEY);
-    //         console.log(plotData);
-
-    //     },
-    //     simpleSheet: true
-    // });
-
-    // var parseSIC = function(SIC_KEY) {
-    //     var plotData = null
-    //     $.ajax({
-    //         'async': false,
-    //         'global': false,
-    //         'url': "http://localhost:8080/SIC.json",
-    //         'dataType': "json",
-    //         'success': function(data) {
-    //             // json = data;
-    //             // console.log(json);
-    //             for (var i = 0; i < data.length; i++) {
-    //                 var sic = data[i].SIC;
-    //                 console.log(SIC_KEY[sic]);
-    //                 var activity = SIC_KEY[sic]["activity"];
-    //                 var resource = SIC_KEY[sic]["resource"];
-    //                 data[i]["activity"] = activity;
-    //                 data[i]["resource"] = resource;
-    //             }
-    //             plotData = data;
-    //         }
-    //     }).done(function() {
-
-    //     });
-    // }
     var margin = { top: 80, right: 20, bottom: 20, left: 80 };
 
     var width = 500 - margin.left - margin.right;
+    // calculate height to keep grid items square
     var height = width * 12 / 7;
 
+    // create axis scale functions
     var x = d3.scale.ordinal().domain(["A", "B", "C", "D", "E", "F", "G"]).rangePoints([width / 7, width]);
-
     var ydomain = ["1.1", "1.2", "1.3", "2.1", "2.2", "2.3", "3.1", "3.2", "3.3", "4.1", "4.2", "4.3"];
     var y = d3.scale.ordinal().domain(ydomain)
         .rangePoints([height / 12, height]);
 
-    var r = d3.scale.linear().range([10, height / 10]);
+    var r = d3.scale.linear().range([0, height / 12]);
 
     var color = d3.scale.category10();
 
@@ -75,17 +35,13 @@ $(function() {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.json("SIC.json", function(error, data) {
-        if (error) throw error;
-        var maxFirmCount = 0;
-        data.forEach(function(d, i) {
-            if (d.activity.length > 1) {
-                var activities = d.activity.replace(/\s+/g, '').split(",");
-                var resources = d.resource.replace(/\s+/g, '').split(",");
-                if (d.year == 2013) {
-                    maxFirmCount = Math.max(maxFirmCount, d.firms / activities.length);
-                }
 
+    var data;
+    d3.json("SIC.json", function(error, read_data) {
+        read_data.forEach(function(d, i) {
+            var activities = d.activity.replace(/\s+/g, '').split(",");
+            var resources = d.resource.replace(/\s+/g, '').split(",");
+            if (d.activity.length > 1) {
                 for (var i = 0; i < activities.length; i++) {
                     var entry = {
                             "year": d.year,
@@ -94,17 +50,25 @@ $(function() {
                             "resource": resources[i],
                             "firms": d.firms / activities.length
                         }
-                    d.firms = 0;      // "remove" the conjugated entry
-                    data.push(entry);
+                        // console.log(data[i]); 
+                    read_data.push(entry);
                 }
-            } else {
-                if (d.year == 2013) {
-                    maxFirmCount = Math.max(maxFirmCount, d.firms);
-                }
-                
+                // data[i].firms = 0; // "remove" the conjugated entry
+                read_data.splice(i, 1);
             }
         });
-        console.log(maxFirmCount);
+        data = read_data;
+
+        var maxFirmCount = 0;
+        var totalFirmCount = 0;
+        data.forEach(function(d, i) {
+            if (d.year == 2013) {
+                console.log(d.firms);
+                maxFirmCount = Math.max(maxFirmCount, d.firms);
+                totalFirmCount += d.firms;
+            }
+        });
+        console.log(totalFirmCount);
         r.domain([0, maxFirmCount]);
 
         svg.append("g")
@@ -119,9 +83,7 @@ $(function() {
             .text("Resource");
 
 
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis)
+        svg.append("g").attr("class", "y axis").call(yAxis)
             .append("text")
             .attr("class", "label")
             .attr("y", -10)
@@ -129,7 +91,6 @@ $(function() {
             .style("text-anchor", "end")
             .text("Activity");
 
-        // console.log(data);
         svg.selectAll(".dot")
             .data(data)
             .enter().append("circle")
@@ -159,4 +120,11 @@ $(function() {
             .style("font-family", "avenir");
 
     });
-})
+
+
+});
+
+
+// d3.json("SIC.json", function(error, data) {
+// if (error) throw error;
+// })
